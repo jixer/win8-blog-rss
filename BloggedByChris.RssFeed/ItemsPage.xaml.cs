8c3b559b71b5139ce76b93b4b35c7f4416c51c5a
jixer <me@bloggedbychris.com>
2013-02-19 19:42:49 -0800

    Initial upload

---
+++ BloggedByChris.RssFeed/ItemsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace BloggedByChris.RssFeed
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class ItemsPage : BloggedByChris.RssFeed.Common.LayoutAwarePage
+    {
+        public ItemsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            this.DefaultViewModel["Items"] = FeedDataSource.GlobalDataSource.Feeds;
+            if (FeedDataSource.GlobalDataSource.Feeds == null || FeedDataSource.GlobalDataSource.Feeds.Count == 0)
+                FeedDataSource.GlobalDataSource.GetFeedsAsync();
+        }
+
+        private void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var groupId = 0;
+            this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
+    }
+}
